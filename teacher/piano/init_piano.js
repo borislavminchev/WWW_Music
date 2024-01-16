b63e53acabe8c118f4fed1be96cc406722352681
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2024-01-16 19:06:32 +0200

    Awesome piano can now be moved

--- teacher/piano/init_piano.js
+++ teacher/piano/init_piano.js
@@ -4,27 +4,25 @@ function isBlackKey(index) {
     return pattern[normalizedIndex] === 1;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const audio = new Audio('../../25405__tedagame__88-piano-keys-long-reverb/448613__tedagame__e4.ogg');
-    const pianoTones = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-    function playSound() {
-      audio.currentTime = 0;
-      audio.play();
-    }
+const audio = new Audio('../../25405__tedagame__88-piano-keys-long-reverb/448613__tedagame__e4.ogg');
+const pianoTones = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+function playSound() {
+    audio.currentTime = 0;
+    audio.play();
+}
 
-    const pianoKeys = 88;
-    const startingKey = 'A'
-    const startingIndex = pianoTones.indexOf(startingKey);
-    const pianoContainer = document.createElement('section');
-    pianoContainer.classList.add('piano-section')
+const pianoKeys = 88;
+const startingKey = 'A'
+const startingIndex = pianoTones.indexOf(startingKey);
+const pianoContainer = document.createElement('section');
+pianoContainer.classList.add('piano-section')
 
-    for (let i = startingIndex; i < pianoKeys + startingIndex; i++) {
-      const button = document.createElement('button');
-      button.innerHTML = `${pianoTones[i%pianoTones.length]}`
-      button.addEventListener('click', playSound);
-      button.classList.add(isBlackKey(i) ? 'black-key' : 'white-key');
-      pianoContainer.appendChild(button);
-    }
+for (let i = startingIndex; i < pianoKeys + startingIndex; i++) {
+    const button = document.createElement('button');
+    button.innerHTML = `${pianoTones[i%pianoTones.length]}`
+    button.addEventListener('click', playSound);
+    button.classList.add(isBlackKey(i) ? 'black-key' : 'white-key');
+    pianoContainer.appendChild(button);
+}
 
-    document.body.appendChild(pianoContainer);
-});
+document.body.appendChild(pianoContainer);
