f43ae0743099e7fa054adf2b00fb62337c1b4653
miteto108 <dimitarangelov2001@abv.bg>
2024-01-17 11:33:48 +0200

    renamed sounds, small change to playsound()

--- teacher/piano/piano_teacher.js
+++ teacher/piano/piano_teacher.js
@@ -1,20 +1,18 @@
 function playSound() {
-    // const response1 = await fetch('C:/Users/Bobby/Downloads/25405__tedagame__88-piano-keys-long-reverb/448613__tedagame__e4.ogg');
-    // const buffer1 = await response1.arrayBuffer();
 
-    // const response2 = await fetch('C:/Users/Bobby/Downloads/25405__tedagame__88-piano-keys-long-reverb/448592__tedagame__g4.ogg');
-    // const buffer2 = await response2.arrayBuffer();
+    let volValue=0.5;
 
-    // const response3 = await fetch('C:/Users/Bobby/Downloads/25405__tedagame__88-piano-keys-long-reverb/448536__tedagame__b4.ogg');
-    // const buffer3 = await response2.arrayBuffer();
-
-    const audio1 = new Audio('../../25405__tedagame__88-piano-keys-long-reverb/448613__tedagame__e4.ogg');
-    const audio2 = new Audio('../../25405__tedagame__88-piano-keys-long-reverb/448592__tedagame__g4.ogg');
-    const audio3 = new Audio('../../25405__tedagame__88-piano-keys-long-reverb/448536__tedagame__b4.ogg');
+    const audio1 = new Audio('../../piano-keys/c4.ogg');
+    const audio2 = new Audio('../../piano-keys/e4.ogg');
+    const audio3 = new Audio('../../piano-keys/g4.ogg');
 
     audio1.currentTime = 0.1;
     audio2.currentTime = 0.1;
     audio3.currentTime = 0.1;
+
+    // audio1.volume = parseFloat(volValue);
+    // audio2.volume = parseFloat(volValue);
+    // audio3.volume = parseFloat(volValue);
 
     audio1.play();
     audio2.play();
@@ -25,6 +23,26 @@ function playSound() {
         audio2.pause();
         audio3.pause();
     }, 1000)
+
+    // audio1.currentTime = 0.1;
+    // audio2.currentTime = 0.1;
+    // audio3.currentTime = 0.1;
+
+    // setTimeout(() => {
+    //     audio1.play();
+    //     audio2.play();
+    //     audio3.play();
+    // }, 1500)
+
+
+    // setTimeout(() => {
+    //     audio1.pause();
+    //     audio2.pause();
+    //     audio3.pause();
+    // }, 1000)
+
+
+
     
     console.log('ok');
 }
