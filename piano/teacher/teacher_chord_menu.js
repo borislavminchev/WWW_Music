39b90e8fd5c9425daeac067b0af390944f7f0e4c
miteto108 <dimitarangelov2001@abv.bg>
2024-02-07 13:21:37 +0200

    mitko commit

--- piano/teacher/teacher_chord_menu.js
+++ piano/teacher/teacher_chord_menu.js
@@ -27,6 +27,16 @@ for(let i=0;i<12;i++){
     optionMin.value = pianoTones[i]+'m';
     TeachDropdown.appendChild(optionMin);
 
+    let option2= document.createElement('option');
+    option2.text = pianoTones[i]+'2';
+    option2.value = pianoTones[i]+'2';
+    TeachDropdown.appendChild(option2);
+
+    let option4 = document.createElement('option');
+    option4.text = pianoTones[i]+'4';
+    option4.value = pianoTones[i]+'4';
+    TeachDropdown.appendChild(option4);
+
 }
 
 // async function retriveChordNotes(chord) {
@@ -58,11 +68,12 @@ async function DisplayTeacher(){
         const button = document.createElement('button');
         button.addEventListener('click', () => closeTeacher());
         button.innerHTML = 'Close';
+        button.className='btn0';
         locContainer.appendChild(button);
 
         let innerContainer = document.createElement('section');
         let chordTitle=document.createElement('h3');
-        chordTitle.textContent=TeachDropdown.value+' chord';
+        chordTitle.textContent=" ' "+TeachDropdown.value+" chord '";
 
         innerContainer.appendChild(chordTitle);
 
@@ -85,18 +96,21 @@ async function DisplayTeacher(){
         //chord = chord.map(chord => chord.replace("#", "-"));
         
         btn1.classList.add('demo-button');
+        btn1.className='btn';
         btn1.textContent = "( "+chord[0]+", "+chord[1]+", "+chord[2]+" )";
-        btn1.addEventListener('click', () => playDemoChord(chord,chord[0],checkbox1.checked,BassDropdown.value,checkbox2.checked,0));
+        btn1.addEventListener('click', () => playDemoChord(chord,checkbox1.checked,BassDropdown.value,checkbox2.checked,0));
         innerContainer.appendChild(btn1);
         let btn2=document.createElement('button');
         btn2.classList.add('demo-button');
+        btn2.className='btn';
         btn2.textContent="( "+chord[1]+", "+chord[2]+", "+chord[0]+" )";
-        btn2.addEventListener('click', () => playDemoChord(chord,chord[1],checkbox1.checked,BassDropdown.value,checkbox2.checked,1));
+        btn2.addEventListener('click', () => playDemoChord(chord,checkbox1.checked,BassDropdown.value,checkbox2.checked,1));
         innerContainer.appendChild(btn2);
         let btn3=document.createElement('button');
         btn3.classList.add('demo-button');
+        btn3.className='btn';
         btn3.textContent="( "+chord[2]+", "+chord[0]+", "+chord[1]+" )";
-        btn3.addEventListener('click', () => playDemoChord(chord,chord[2],checkbox1.checked,BassDropdown.value,checkbox2.checked,2));
+        btn3.addEventListener('click', () => playDemoChord(chord,checkbox1.checked,BassDropdown.value,checkbox2.checked,2));
         innerContainer.appendChild(btn3);
 
         var checkbox1 = document.createElement('input');
@@ -152,27 +166,86 @@ function closeTeacher(){
 
 }
 
-function playDemoChord(chord,begg_chord,bass,begg_bass,harmony,inversion){// (array notes,note,bool,note,bool)
+const letterToNumberMap = {
+    'C': 0,'C#': 1,'D': 2,'D#': 3,'E': 4,'F': 5,    
+    'F#': 6,'G': 7,'G#': 8,'A': 9,'A#': 10,'B': 11
+  };
+function compareLetters(letter1, letter2) {
+    const number1 = letterToNumberMap[letter1];
+    const number2 = letterToNumberMap[letter2];
+    return number1<number2;
+}
 
-    //generic
-    //playNote('e3',8.5*timePl,1.0,br*(timePl+5));
-    if(inversion==0){
-        playNote(chord[0]+'4',4000,0.8,0);
-        playNote(chord[1]+'4',4000,0.8,0);
-        playNote(chord[2]+'4',4000,0.8,0);
-    }
-    if(inversion==1){
-        playNote(chord[1]+'4',4000,0.8,0);
-        playNote(chord[2]+'4',4000,0.8,0);
-        playNote(chord[0]+'5',4000,0.8,0);
+function playDemoChord(chord,bass,begg_bass,harmony,inversion){// (array notes,note,bool,bool,mode)
 
+    let note0,note1,note2,other=0;
+    if (compareLetters(chord[0],chord[1])){
+        if(compareLetters(chord[1],chord[2])){
+            note0=0;
+            note1=1;
+            note2=2;
+            other=0;
+            console.log("0");
+        }
+        else{
+            if(compareLetters(chord[0],chord[2])){
+                note0=0;
+                note1=2;
+                note2=1;
+                other=1;
+                console.log("1");
+                //console.log("err");
+            }
+            note0=1;
+            note1=2;
+            note2=0;
+            other=1;
+            console.log("1*");
+        }
     }
-    if(inversion==2){
-        playNote(chord[2]+'4',4000,0.8,0);
-        playNote(chord[0]+'5',4000,0.8,0);
-        playNote(chord[1]+'5',4000,0.8,0);
+    else{
+        if(compareLetters(chord[0],chord[2])){
+            note0=1;
+            note1=0;
+            note2=2;
+            other=2;
+            console.log("2*");
+            //console.log("err");
+        }
+        else{
+            if(compareLetters(chord[1],chord[2])){
+                note0=2;
+                note1=0;
+                note2=1;
+                other=2;
+                console.log("2");
+            }
+            note0=2;
+            note1=1;
+            note2=0;
+            other=0;
+            console.log("0*");
+        }
     }
-
+    console.log("inversion-"+inversion+" other-"+other);
+    console.log("note0="+note0+" note1="+note1+" note2="+note2);
+    invother=inversion+other;
+    //invother=inversion;
+    if(invother%3==0){
+        playNote(chord[note0]+'4',4000,0.8,0);
+        playNote(chord[note1]+'4',4000,0.8,0);
+        playNote(chord[note2]+'4',4000,0.8,0);        
+    }
+    if(invother%3==1){
+        playNote(chord[note1]+'4',4000,0.8,0);
+        playNote(chord[note2]+'4',4000,0.8,0);
+        playNote(chord[note0]+'5',4000,0.8,0);
+    }
+    if(invother%3==2){
+        playNote(chord[note2]+'4',4000,0.8,0);
+        playNote(chord[note0]+'5',4000,0.8,0);
+        playNote(chord[note1]+'5',4000,0.8,0);
+    }
 
     if(bass){
         playNote(begg_bass+'2',4000,0.8,0);
@@ -180,15 +253,14 @@ function playDemoChord(chord,begg_chord,bass,begg_bass,harmony,inversion){// (ar
     }
 
     if(harmony){
-        playNote(chord[0]+'5',3000,0.8,2000);
-        playNote(chord[1]+'5',2800,0.8,2200);
-        playNote(chord[2]+'5',2600,0.8,2400);
-        playNote(chord[0]+'6',2400,0.8,2600);
-        playNote(chord[1]+'6',2200,0.8,2800);
-        playNote(chord[2]+'6',2000,0.8,3000);
-        playNote(chord[0]+'7',1800,0.8,3200);
+        playNote(chord[note0]+'5',3000,0.8,2000);
+        playNote(chord[note1]+'5',2800,0.8,2200);
+        playNote(chord[note2]+'5',2600,0.8,2400);
+        playNote(chord[note0]+'6',2400,0.8,2600);
+        playNote(chord[note1]+'6',2200,0.8,2800);
+        playNote(chord[note2]+'6',2000,0.8,3000);
+        playNote(chord[note0]+'7',1800,0.8,3200);
 
     }
-
 }
 
